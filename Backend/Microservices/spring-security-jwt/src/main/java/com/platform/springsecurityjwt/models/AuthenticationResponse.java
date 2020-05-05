9eebf02e4c044b0ec89fc8578704e312697937b3
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-05-05 23:54:59 +0530

    Spring JWT inMemory Implementation

---
+++ Backend/Microservices/spring-security-jwt/src/main/java/com/platform/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.platform.springsecurityjwt.models;
+
+public class AuthenticationResponse {
+	private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+}
---
+++ Backend/Microservices/spring-security-jwt/src/main/java/com/platform/springsecurityjwt/services/MyUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.platform.springsecurityjwt.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new User("foo","bar",new ArrayList<>());
+	}
+	
+}
